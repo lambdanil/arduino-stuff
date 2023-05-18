7638160472b0b9a3ce55d19e3c324cba536af5db
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2023-05-18 09:22:13 +0200

    Add files via upload

---
+++ keypass/sketch.ino
@@ -0,0 +1,308 @@
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+
+#define LED                    13    // Relay pin
+
+byte    PIN_LEN              = 4;    // Length of PIN code
+                                     // Not defined as constant so it can be changed
+
+const long WAIT_MS           = 5000; // time before locking again
+enum METHODS{WAIT, CONFIRM};
+enum METHODS METHOD          = WAIT; // handle re-locking after an unlock, either by a delay or by user confirm
+
+const byte rows              = 4;
+const byte cols              = 4;
+const byte colPins[rows]     = {5, 4, 3, 2};
+const byte rowPins[cols]     = {9, 8, 7, 6};
+const byte lcd_addr          = 0x27;
+const byte lcd_cols          = 16;
+const byte lcd_rows          = 2;
+
+const char keys[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+Keypad keypad = Keypad(
+  makeKeymap(keys),
+  rowPins,
+  colPins,
+  rows,
+  cols
+);
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(lcd_addr, lcd_cols, lcd_rows);
+
+void setup() {
+  //Serial.begin(9600);
+  lcd.begin(lcd_addr, lcd_cols, lcd_rows);  
+  lcd.setBacklight(100);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+// Reset password string to "----", returns new string if 'str' isn't specified
+char* strunset(char* str = NULL, byte strlen = PIN_LEN) {
+  if (str == NULL)
+    str = malloc(sizeof(char) * (strlen+1));
+  for (int i = 0; i < strlen; i++) {
+    str[i] = '-';
+  }
+  str[strlen] = '\0';
+  return str;
+}
+
+// Center text in row
+void lcd_center(char* str, int row) {
+  lcd.setCursor(lcd_cols / 2 - strlen(str) / 2, row);
+  lcd.print(str);
+}
+
+
+// Clear the entered pasword
+void reset_pass(char *current, byte& index, bool wrong=false) {
+  index = 0;
+  current = strunset(current);
+  lcd.clear();
+  if (wrong) {
+    lcd_center("Wrong password!", lcd_rows / 2 - 1);
+    delay(2000);
+  }
+  lcd.clear();
+}
+
+void unlock() {
+  digitalWrite(LED, HIGH);
+  lcd.clear();
+  lcd_center("unlocked", lcd_rows / 2);
+}
+
+void lock() {
+  digitalWrite(LED, LOW);
+}
+
+int int_from_pinstr(char* str) {
+  int r = 0;
+  const int len = strlen(str);
+  for (int i = 0; str[i] != '-' && i < len; i++) {
+    r = r * 10 + (str[i] - 48);
+  }
+  if (strlen(str))
+    return r;
+  else 
+    return 5;
+}
+
+void get_pin_from_keypad(char* str, char* pass, byte len=PIN_LEN) {
+  char key;
+  char* current = strunset(current, len);
+  byte index = 0;
+  lcd.clear();
+  while (true) {
+    key = keypad.getKey();
+    lcd_center(str, lcd_rows / 2 - 1);
+    lcd_center(current, lcd_rows / 2);
+    if (isdigit(key)) {
+      if (index<len) {
+        current[index] = key;
+        index++;
+      }
+    }
+    else if (key == 'C') {
+      current = strunset(current, len);
+      index = 0;
+    }
+    else if (key == 'D') {
+      pass[len] = '\0';
+      strncpy(pass, current, len);
+      current = strunset(current, len);
+      index = 0;
+      lcd.clear();
+      free(current);
+      return;
+    }
+  }
+}
+
+void menu(char* pass, long& wait) {
+  static const char* items[] = {"set PIN", "timeout mode", "set timeout", "set PIN len"};
+  const byte items_len = sizeof(items) / sizeof(char*);
+  byte index = 0;
+  char* w_s;
+  byte n_i, c_pos;
+  bool redraw = true;
+  byte last = 0;
+  char key;
+  lcd.clear();
+  while (true) {
+    key = keypad.getKey();
+    if (key == 'A') { // move up
+      if (index > 0) index--;
+    }
+    else if (key == 'B') { // move down
+      if (index < items_len-1) index++;
+    }
+    else if (key == 'D') { 
+      if (index == 0) { // set new pin
+        get_pin_from_keypad("Set PIN:", pass);
+        redraw = true;
+      }
+      else if (index == 1) { // switch unlock mode
+        if (METHOD == WAIT) {
+          METHOD = CONFIRM;
+          lcd.clear();
+          lcd_center("timeout: off", lcd_rows / 2 - 1);
+          delay(1500);
+          redraw = true;
+        }
+        else if (METHOD == CONFIRM) {
+          METHOD = WAIT;
+          lcd.clear();
+          lcd_center("timeout: on", lcd_rows / 2 - 1);
+          delay(1500);
+          redraw = true;
+        }
+      }
+      else if (index == 2) { // set new delay
+        w_s = strunset();
+        get_pin_from_keypad("Set timeout (s):", w_s, 4);
+        wait = int_from_pinstr(w_s) * 1000L;
+        free(w_s);
+        redraw = true;
+      }
+      else if (index == 3) { // set length of PIN
+        do {
+          w_s = strunset();
+          get_pin_from_keypad("Set PIN length:", w_s, 1);
+          PIN_LEN = int_from_pinstr(w_s);
+          free(w_s);
+        } while (!(PIN_LEN > 0 && PIN_LEN <= 9));
+
+        get_pin_from_keypad("Set PIN:", pass);
+
+        redraw = true;
+      }
+    }
+    else if (key == '*') {
+      lcd.clear();
+      return;
+    }
+
+    if (last != index || redraw) {
+      lcd.clear();
+      n_i = index;
+      if (index >= items_len - lcd_rows) n_i = items_len - lcd_rows;
+      for (int i = 0; i < lcd_rows; i++) {
+        lcd.setCursor(3, i);
+        lcd.print(items[n_i+i]);
+      }
+
+      c_pos = index-n_i;
+      lcd.setCursor(0, c_pos);
+      lcd.write('-');
+      lcd.write('>');
+    }
+    if (redraw) redraw = false;
+    last = index;
+  }
+}
+
+void loop() {
+  static char* current    = strunset(); // we never need to free these strings
+  static char* pass       = strunset();
+  static long  wait       = 5000;
+  static bool  pass_set   = false;
+  static byte  index      = 0;
+  static char  key;
+  static int   failed     = 0;
+  static int   multiplier = 15;
+
+  key = keypad.getKey();
+
+  /* --- Set the initial password --- */
+  if (!pass_set) {
+    get_pin_from_keypad("Set PIN:", pass);
+    pass_set = true;
+  }
+  /* -------------------------------- */
+
+  if (key == 'D' && !strcmp(current, pass)) { // unlock
+    failed = 0;
+    multiplier = 15;
+    unlock();
+    if (METHOD == WAIT) {
+      char t[5];
+      lcd.clear();
+      lcd_center("* lock  # menu", lcd_rows / 2 - 1);
+      long m = millis();
+      long ms;
+      while (millis() < m+wait) {
+        key = keypad.getKey();
+        if (key == '#') {
+          menu(pass, wait);
+          break;
+        }
+        else if (key == '*') break;
+        ms = m+wait-millis();
+        byte center = lcd_cols/2 - 3 / 2;
+        lcd.setCursor(center, 1);
+        itoa(ms/1000, t, 10);
+        lcd_center(t,lcd_rows / 2);
+      }
+    }
+    else if (METHOD == CONFIRM) {
+      lcd_center("* lock  # menu", lcd_rows / 2 - 1);
+      key = '0';
+      while (true) { // wait for lock confirm
+        key = keypad.getKey();
+        if (key == '*') {
+          break;
+        }
+        else if (key == '#') {
+          menu(pass, wait);
+          break;
+        }
+      }
+    }
+
+    lock();
+    reset_pass(current, index);
+  }
+  else if (isdigit(key)) {
+    if (index<PIN_LEN) {
+      current[index] = key;
+      index++;
+    }
+  }
+
+
+  else if (key == 'C') { // clear password
+    reset_pass(current, index);
+  }
+
+
+  else if (key == 'D') { // failed unlock
+    reset_pass(current, index, true);
+    failed++;
+    if (failed > 2) { // anti-bruteforce wait after 3 failed attempts
+      lcd.clear();
+      lcd.println("No bruteforcing!");
+      long d = 1000 * multiplier;
+      multiplier*=2;
+      long m = millis();
+      while (millis() < m+d) {
+        int ms = m+d-millis();
+        lcd.setCursor(lcd_cols/2 - 3 / 2, 1);
+        lcd.println(ms/1000);
+        m++;
+      }
+      failed = 0;
+    }
+    reset_pass(current, index, false);
+  }
+
+  lcd_center(current, lcd_rows / 2 - 1);
+  lcd_center("locked", lcd_rows / 2);
+}
---
+++ snake/sketch.ino
@@ -0,0 +1,308 @@
+/* 
+ * Copyright (c) 2023 Jan Novotný.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <LiquidCrystal_I2C.h>
+
+#define B_R 2 // buttons for direction
+#define B_L 0
+#define B_U 4
+#define B_D 3
+#define DISPLAY_H 4 // display height
+#define DISPLAY_W 20 // display width
+#define DISPLAY_ADDR 0x27 // I2C display address
+#define MS_WAIT 300
+
+struct pos {
+  unsigned int x;
+  unsigned int y;
+};
+
+typedef struct Vector {
+    size_t size; // number of elements in vector
+    struct pos* at; // dynamic array
+} vector;
+
+void vector_init(vector *cvector, size_t n) {
+    cvector->at = (struct pos*)malloc(n * sizeof(struct pos)); // allocate memory
+    cvector->size = n;
+}
+
+void vector_resize(vector *cvector, size_t n) {
+    cvector->at = (struct pos*)realloc(cvector->at, n * sizeof(struct pos)); // reallocate memory
+    cvector->size = n; 
+}
+
+void vector_push(vector *cvector, struct pos toPush) {
+    size_t nsize = (cvector->size+1) ; // increase number of elements by one
+    vector_resize(cvector, nsize);
+    cvector->at[(cvector->size)-1] = toPush; // insert the value
+}
+
+void vector_insert(vector *cvector, struct pos toPush, int n) {
+    size_t nsize = (cvector->size+1) ; // increase number of elements by one
+    vector_resize(cvector, nsize);
+    for (int i = (int)cvector->size - 1; i > n; i--) {
+        cvector->at[i] = cvector->at[i-1]; // move array values
+    } 
+    cvector->at[n] = toPush; // insert the value
+}
+
+void vector_free(vector *cvector) {
+    free(cvector->at);
+}
+
+void vector_delete_last(vector *cvector) {
+    vector_resize(cvector, cvector->size - 1); // deallocate last cell
+}
+
+void vector_delete_at(vector *cvector, int n) { // delete specific value
+    for (int i = n; i < (int)cvector->size - 1; i++) {
+        cvector->at[i] = cvector->at[i+1]; // move array values
+    }
+    vector_resize(cvector, cvector->size - 1); // resize array
+}
+
+LiquidCrystal_I2C LCD(DISPLAY_ADDR, DISPLAY_W, DISPLAY_H);
+
+String pad_text(String, String fill=" ", int max=DISPLAY_W);
+
+#define WIDTH DISPLAY_W
+#define HEIGHT DISPLAY_H
+#define HEAD_CHAR 'X'
+#define SEG_CHAR 'x'
+#define FOOD_CHAR 'o'
+#define FLOOR_CHAR ' '
+#define BORDER_CHAR '#'
+
+struct scr_buf {
+  const unsigned int width = WIDTH;
+  const unsigned int height = HEIGHT;
+  char buf[HEIGHT][WIDTH];
+};
+
+struct game_state {
+  struct scr_buf screen;
+  struct pos head;
+  struct pos food;
+  vector segments;
+  char dir; // 'u', 'l', 'd', 'r'
+  char l_dir;
+  bool add_segment_next_loop = false;
+};
+
+void move_food(struct game_state& game) {
+  game.food.x = random(0, game.screen.width - 1);
+  game.food.y = random(0, game.screen.height - 1);
+  while ((game.screen.buf[game.food.y][game.food.x] != FLOOR_CHAR) ||
+        (game.food.x == game.segments.at[game.segments.size-1].x &&
+         game.food.y == game.segments.at[game.segments.size-1].y)) {
+    game.food.x = random(0, game.screen.width - 1);
+    game.food.y = random(0, game.screen.height - 1); 
+  }
+}
+
+String pad_text(String text, String fill=" ", int max=DISPLAY_W) {
+  String ftext = text;
+  while (ftext.length() < max) {
+    ftext = fill + ftext;
+    ftext += fill;
+  }
+  return ftext;
+}
+
+void game_over(struct game_state& game) {
+  LCD.setCursor(0,DISPLAY_H/2-1);
+  LCD.print(pad_text("Game over!"));
+  LCD.setCursor(0,DISPLAY_H/2);
+  LCD.print(pad_text("Final score:" + String(game.segments.size-1)));
+  exit(0);
+}
+
+void next_frame(struct game_state& game) {
+  struct scr_buf& screen = game.screen;
+  bool& add_segment_next_loop = game.add_segment_next_loop;
+  static struct pos last_segment;
+  static bool first_run = true;
+
+  if (game.segments.size) {
+    struct pos new_segment;
+    new_segment.x = game.segments.at[game.segments.size - 1].x;
+    new_segment.y = game.segments.at[game.segments.size - 1].y;
+    for (int i = game.segments.size - 1; i > 0; i--) {
+      game.segments.at[i] = game.segments.at[i - 1];
+    }
+    game.segments.at[0] = game.head;
+    if (add_segment_next_loop) {
+      vector_push(&game.segments, new_segment);
+      add_segment_next_loop = false;
+    }
+  }
+
+  if (game.head.x == game.food.x && game.head.y == game.food.y) {
+    add_segment_next_loop = true;
+    move_food(game);
+  }
+
+  switch (game.dir) {
+    case 'u':
+      if (game.head.y == 0)
+        game_over(game);
+      game.head.y--;
+      break;
+    case 'd':
+      if (game.head.y == game.screen.height - 1)
+        game_over(game);
+      game.head.y++;
+      break;
+    case 'r':
+      if (game.head.x == game.screen.width - 1)
+        game_over(game);
+      game.head.x++;
+      break;
+    case 'l':
+      if (game.head.x == 0)
+        game_over(game);
+      game.head.x--;
+      break;
+  }
+
+  if (game.segments.size) {
+    if (first_run) {
+      first_run = false;
+    }
+
+    if (!game.add_segment_next_loop) {
+      screen.buf[last_segment.y][last_segment.x] = FLOOR_CHAR;
+      last_segment.x = game.segments.at[game.segments.size - 1].x;
+      last_segment.y = game.segments.at[game.segments.size - 1].y;
+    }
+
+    screen.buf[game.segments.at[0].y][game.segments.at[0].x] = SEG_CHAR;
+    screen.buf[game.segments.at[game.segments.size-1].y][game.segments.at[game.segments.size-1].x] = SEG_CHAR;
+  }
+
+  if (!(game.head.x == game.segments.at[0].x && game.head.y == game.segments.at[0].y)
+      &&
+      screen.buf[game.head.y][game.head.x] == SEG_CHAR && !first_run)
+    game_over(game);
+
+  if (game.head.x == game.food.x && game.head.y == game.food.y) {
+    add_segment_next_loop = true;
+    move_food(game);
+  }
+}
+
+void fill_screen(struct scr_buf& screen, char fill) {
+  for (unsigned int i = 0; i < screen.height; i++) {
+    for (unsigned int j = 0; j < screen.width; j++) {
+      screen.buf[i][j] = fill;
+    }
+  }
+}
+
+void fill_segments(struct game_state& game) {
+  for (int i = game.segments.size - 1; i > 0; i--) {
+    game.screen.buf[game.segments.at[i].y][game.segments.at[i].x] = SEG_CHAR;
+  }
+}
+
+void init_game(struct game_state& game) {
+  game.head.x = game.screen.width / 2;
+  game.head.y = game.screen.height / 2;
+  fill_screen(game.screen, FLOOR_CHAR);
+  fill_segments(game);
+  move_food(game);
+  struct pos first_segment;
+  first_segment.x = game.head.x;
+  first_segment.y = game.head.y;
+  vector_init(&game.segments, 0);
+  vector_push(&game.segments, first_segment);
+}
+
+void print_screen(struct game_state& game) {
+  struct scr_buf& screen = game.screen;
+  String out = "";
+
+  LCD.setCursor(0, 0);
+  int li;
+  for (unsigned int i = 0; i < screen.height; i++) {
+    if (li != i) {
+      LCD.setCursor(0, li);
+      LCD.print(out);
+      out = "";
+    }
+    for (unsigned int j = 0; j < screen.width; j++) {
+      if (i == game.head.y && j == game.head.x)
+        out += HEAD_CHAR;
+      else if (i == game.food.y && j == game.food.x)
+        out += FOOD_CHAR;
+      else if (screen.buf[i][j] == SEG_CHAR)
+        out += SEG_CHAR;
+      else
+        out += screen.buf[i][j];
+    }
+    li = i;
+  }
+  LCD.setCursor(0, li);
+  LCD.print(out);
+} 
+
+void eval_key(struct game_state& game) {
+    if (!digitalRead(B_R) && game.l_dir != 'l') {
+      game.dir = 'r';
+    }
+    if (!digitalRead(B_L) && game.l_dir != 'r') {
+      game.dir = 'l';
+    }
+    if (!digitalRead(B_U) && game.l_dir != 'd') {
+      game.dir = 'u';
+    }
+    if (!digitalRead(B_D) && game.l_dir != 'u') {
+      game.dir = 'd';
+    }
+}
+
+struct game_state game;
+
+void setup() {
+  pinMode(B_R, INPUT_PULLUP);
+  pinMode(B_L, INPUT_PULLUP);
+  pinMode(B_D, INPUT_PULLUP);
+  pinMode(B_U, INPUT_PULLUP);
+  LCD.begin(DISPLAY_W, DISPLAY_H);
+  LCD.backlight();
+  LCD.noBlink();
+  randomSeed(analogRead(A0));
+  //srand(time(0));
+  init_game(game);
+
+  char mv;
+}
+
+void loop() {
+  static int counter = 0;
+
+  if(counter == MS_WAIT) {
+    next_frame(game);
+    print_screen(game);
+    game.l_dir = game.dir;
+  }
+
+  eval_key(game);
+
+  counter++;
+  if (counter > MS_WAIT) counter = 0;
+  delay(1);
+}
